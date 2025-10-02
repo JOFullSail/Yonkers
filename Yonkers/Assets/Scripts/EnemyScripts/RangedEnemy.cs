6b648911fd638f920b2c3b29657e5bbffa750cd8
JOFullSail <jtolsen@student.fullsail.edu>
2025-10-02 19:32:03 -0400

    Damage and Explosions

--- Yonkers/Assets/Scripts/EnemyScripts/RangedEnemy.cs
+++ Yonkers/Assets/Scripts/EnemyScripts/RangedEnemy.cs
@@ -4,6 +4,14 @@ public class RangedEnemy : EnemyAI
 {
     [SerializeField] int weaponRange;
 
+    [SerializeField] GameObject projectile;
+    [SerializeField] Transform shootPos;
+    [SerializeField] float delayBetweenShots;
+
+    float shotTimer;
+
+    bool playerInWeaponRange;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +21,19 @@ public class RangedEnemy : EnemyAI
     // Update is called once per frame
     void Update()
     {
-        
+        shotTimer += Time.deltaTime;
+
+        if (playerInWeaponRange)
+        {
+            if (shotTimer > delayBetweenShots)
+                shoot();
+        }
+    }
+
+    void shoot()
+    {
+        shotTimer = 0;
+
+        Instantiate(projectile, shootPos.position, transform.rotation);
     }
 }
