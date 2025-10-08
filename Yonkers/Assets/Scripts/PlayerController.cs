e3ad682e1b8df0ec41af1e581aca64775b0def9a
jpfso <jpaganbaez@student.fullsail.edu>
2025-10-08 02:58:10 -0400

    Finished setting up Main Scene for First Level
    It took me an ungodly amount of time to fix a particular error

--- Yonkers/Assets/Scripts/PlayerController.cs
+++ Yonkers/Assets/Scripts/PlayerController.cs
@@ -1,212 +1,163 @@
 using UnityEngine;
 
-
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IDamage
 {
-    [SerializeField] LayerMask ignoreLayer;
     [SerializeField] CharacterController controller;
-    [SerializeField] float Speed0;
-    [SerializeField] float MinSpeed;
-    [SerializeField] float MaxSpeed;
-    [SerializeField] float MinAccel;
-    [SerializeField] float MinDeaccel;
-    [SerializeField] float MaxAccel;
-    [SerializeField] float MaxDeaccel;
-    [SerializeField] float speedAccelRATE;
-    [SerializeField] float speedDeaccelRATE; //how fast the Deceel ramps up
-    [SerializeField] float sprintMod;
-    [SerializeField] int jumpSpeed;
-    [SerializeField] int jumpCountMax;
-    [SerializeField] int gravity;
-    [SerializeField] int shootDamage;
-    [SerializeField] int shootDist;
-    [SerializeField] float shootRate;
-    [SerializeField] Collider slopecheck;
-    float currspeedAccel;
-    float currspeedDeaccel;
-    float speedDeaccelOrig;
-    float speedAccelOrig;
-    float currentSpeed;
-    Vector3 moveDir;
-    Vector3 MomentumDir;
-    Vector3 CameraDir; //here in case you want to use camera as the way of controlling momentum.
+    [SerializeField] LayerMask ignoreLayer;
+
+    [Header("General")]
+    [SerializeField] int HP; // 10 (Could be changed)
+    [SerializeField] int speed; // 12
+
+    [Header("Jumping")]
+    [SerializeField] int jumpSpeed; // 12
+    [SerializeField] int jumpMaxCount; // 2
+    [SerializeField] float jumpGracePeriod; // 0.175
+    [SerializeField] int gravity; // 35
+
+    [Header("Shooting")]
+    [SerializeField] int shootDmg; // 1 (Could be changed)
+    [SerializeField] int shootDist; // 15 (Could be changed)
+    [SerializeField] float shootRate; // 0.5 (Could be changed)
+
+    [Header("Climbing")]
+    [SerializeField] float climbSpeed; // 10.25
+    [SerializeField] int climbingDist; // 0
+    [SerializeField] float climbWallDetection; // 1.25
+    [SerializeField] float climbMaxAngle; // 90
+
+
+    Vector3 moveDirec;
     Vector3 playerVel;
-    Vector3 camfor;
+
+    RaycastHit hit;
+
     int jumpCount;
-    GameObject player;
+    int hpOrig;
+
+    float jumpTimer;
     float shootTimer;
-    bool isSprinting;
-    bool _onSlope;
-    Vector3 camdown = Vector3.down;
+
+    bool isClimbing;
+
+    // - UNUSED -
+
+    //[SerializeField] int sprintMod; // Speed multiplier.
+
+    //bool isSprinting;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
-        camfor = Camera.main.transform.forward;
-        currentSpeed = Speed0;
-        currspeedAccel = MinAccel;
-        currspeedDeaccel = MinDeaccel;
-        player = GameObject.FindWithTag("Player");
-        speedDeaccelOrig = currspeedDeaccel;
-        speedAccelOrig = currspeedAccel;
+        hpOrig = HP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.green);
 
-        Debug.DrawRay(player.transform.position, camdown * shootDist, Color.blue); //ray that looks at the floor
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * climbWallDetection, Color.blue);
+
+        if (!controller.isGrounded) jumpTimer += Time.deltaTime;
+        else jumpTimer = 0;
         shootTimer += Time.deltaTime;
-        Vector3 camwithouty = Camera.main.transform.forward;
-        camwithouty.y = 0;
-        CameraDir  = camwithouty; //no y = no fly!
+
         movement();
-        sprint();
     }
 
     void movement()
     {
+        climb();
+
         if (controller.isGrounded)
         {
-            playerVel = Vector3.zero;
+            playerVel.y = 0;
             jumpCount = 0;
         }
-        else
+        else if (!isClimbing)
         {
-            //currentSpeed += speedAccel; slopes?
             playerVel.y -= gravity * Time.deltaTime;
         }
-        //if (controller.isGrounded && _onSlope == true)
-        //{
 
-        //}
-        if (currentSpeed >= MaxSpeed && (Input.GetButton("Horizontal") == true || Input.GetButton("Vertical") == true)) // Uncomment this block of code to prevent the player from stopping immediately after reaching max speed.
-        {
-            if (currentSpeed > MaxSpeed && Input.GetButton("Shift") == false)
-            {
-                if (currentSpeed > MaxSpeed + 1)
-                {
-                    currentSpeed -= currspeedDeaccel * Time.deltaTime;
-                    Ramp();
-                }
-            }
-            moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-            MomentumDir = moveDir;
-            controller.Move(moveDir * currentSpeed * Time.deltaTime);
-        }
-        else if ((Input.GetButton("Horizontal") == true || Input.GetButton("Vertical") == true) && currentSpeed < MaxSpeed)
-        {
-
-            if (currentSpeed < MinSpeed)
-            {
-                currentSpeed = MinSpeed;
-            }
-            moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-            MomentumDir = moveDir;
-            currentSpeed += currspeedAccel * Time.deltaTime;
-            Ramp();
-            controller.Move(moveDir * currentSpeed * Time.deltaTime);
-        }
-        else if ((Input.GetButton("Horizontal") == false && Input.GetButton("Vertical") == false) && currentSpeed > Speed0)
-        {
-            currentSpeed -= currspeedDeaccel * Time.deltaTime;
-            Ramp();
-            if (currentSpeed < 0)
-            {
-                currentSpeed = 0;
-            }
-            //CameraDir or MomentumDir
-            controller.Move(MomentumDir * currentSpeed * Time.deltaTime);
-        }
-
+        moveDirec = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDirec * speed * Time.deltaTime);
 
         jump();
         controller.Move(playerVel * Time.deltaTime);
+
+        
         if (Input.GetButton("Fire1") && shootTimer >= shootRate)
+        {
             shoot();
+        }
     }
 
-    void sprint()
+    void climb()
     {
-        if (Input.GetButtonDown("Shift"))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, climbWallDetection, ~ignoreLayer))
         {
-            MaxSpeed *= sprintMod;
-            speedAccelRATE *= sprintMod;
-            MinSpeed *= sprintMod;
+            int wallAngle = (int)Vector3.Angle(hit.normal, Vector3.up);
+            if (!hit.collider.CompareTag("NoClimb") && controller.slopeLimit <= wallAngle && wallAngle <= climbMaxAngle && !controller.isGrounded && (playerVel.y < 0 || isClimbing))
+            {
+                playerVel.y = climbSpeed;
+                isClimbing = true;
+            }
         }
-        else if (Input.GetButtonUp("Shift"))
-        {
-            MaxSpeed /= sprintMod;
-            speedAccelRATE /= sprintMod;
-            MinSpeed /= sprintMod;
-        }
+        else isClimbing = false;
     }
 
     void jump()
     {
-        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMaxCount)
         {
+            if (jumpTimer >= jumpGracePeriod && jumpCount == 0)
+            {
+                ++jumpCount;
+            }
+
             playerVel.y = jumpSpeed;
-            jumpCount++;
+            ++jumpCount;
         }
     }
+
+    //void sprint()
+    //{
+    //    if (Input.GetButtonDown("Sprint"))
+    //    {
+    //        speed *= sprintMod;
+    //    }
+    //    else if (Input.GetButtonUp("Sprint"))
+    //    {
+    //        speed /= sprintMod;
+    //    }
+    //}
 
     void shoot()
     {
         shootTimer = 0;
 
-        RaycastHit hit;
+        // ~ignoreLayer will ignore the player later to prevent the player shooting themselves.
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
         {
             IDamage dmg = hit.collider.GetComponent<IDamage>();
             if (dmg != null)
             {
-                dmg.takeDamage(shootDamage);
+                dmg.takeDamage(shootDmg);
             }
 
-            Debug.Log(hit.collider.name);
+            Debug.Log(hit.collider.name); // logs info to the debug status bar.
         }
     }
 
-    void Ramp() //used to make Accel and Deacell higher over time.
-    { 
-        if ((Input.GetButton("Horizontal") == false && Input.GetButton("Vertical") == false) && currentSpeed < MaxSpeed)
-        {
-            currspeedAccel = speedAccelOrig;
-            if (currentSpeed == Speed0)
-            {
-                currspeedDeaccel = speedDeaccelOrig;
-            }else if (currspeedDeaccel < MaxDeaccel)
-            {
-                    currspeedDeaccel += speedDeaccelRATE * Time.deltaTime;
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
 
-            }
-        }
-        else if ((Input.GetButton("Horizontal") == true || Input.GetButton("Vertical") == true) && currentSpeed > Speed0)
+        if (HP <= 0)
         {
-            currspeedDeaccel = speedDeaccelOrig;
-            if (currentSpeed >= MaxSpeed)
-            {
-                currspeedAccel = MaxSpeed;
-            }
-            else if (currspeedAccel < MaxAccel)
-            {
-                currspeedAccel += speedAccelRATE * Time.deltaTime;
-            }
-           
+            GameManager.instance.youDied();
         }
-    } 
-    //bugs: accel goes to speed max upon speed reaching max
-    
-    //bool onslope()
-    //{
-    //    bool check = false;
-    //    RaycastHit slopehit;
-    //    if (Physics.Raycast(player.transform.position, camdown * shootDist, out slopehit, slopecheck.GetComponent<>))
-    //    {
-    //        check = true;
-    //    }
-    //    return check;
-    //}
+    }
 }
