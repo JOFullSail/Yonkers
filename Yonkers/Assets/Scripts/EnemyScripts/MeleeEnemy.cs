b74678a9c075aa57f243041699452dfdde5c56c3
TaquitoBurritoXL <bmanquez1738@gmail.com>
2025-10-14 15:51:03 -0400

    Melee Enemy 1.0
    The Melee Enemy, or YonkPunch, will zip towards you if you get too close to his trigger. If you get close to his reach, he will push you back, potentially pushing you off the ledge while also dealing damage. To counter him, dodge or stay far from him to pick him off.

--- Yonkers/Assets/Scripts/EnemyScripts/MeleeEnemy.cs
+++ Yonkers/Assets/Scripts/EnemyScripts/MeleeEnemy.cs
@@ -1,22 +1,27 @@
-using UnityEditor;
+
+using System.Collections;
 using UnityEngine;
+
 
 
 public class MeleeEnemy : EnemyAI
 {
     [Header("Melee Attributes")]
-    [SerializeField] Transform EnemyPOS;
+    [SerializeField] Transform EnemyPOS;// Enemy transfrom
+    [SerializeField] float Upwardforce; //
     [SerializeField] int MeleeDamage;
     [SerializeField] float MeleeDelay;
     [SerializeField] float DashDistance;
     [SerializeField] float DashSpeed;
     [SerializeField] float TargetDistance;
-    
+    [SerializeField] float Reach;
+
+    bool Collide;
     bool AttackRange;
     float AttackTimer;
     Vector3 PlayerPosition;
     Vector3 NewPushPosition;
-    Vector3 Dir;
+    Vector3 Dir ;
 
     void Update()
     {
@@ -25,19 +30,38 @@ public class MeleeEnemy : EnemyAI
         roamRoutine();
         if (canSeePlayer())
         {
+            
             if (AttackTimer > MeleeDelay)
             {
                 dashattack();
-                AttackTimer = 0;
-            }
-            if (AttackRange == true)
-            {
+
                 transform.position = Vector3.Lerp(transform.position, NewPushPosition, Time.deltaTime * DashSpeed);
+                punch(Upwardforce, (GameManager.instance.player.transform.position - transform.position));
+                
             }
+            
         }
     }
 
-   
+    void punch(float Force, Vector3 Dir)
+    {
+
+        Dir = Dir.normalized;
+        Vector3 TotalPunch = Dir * Force;
+        
+
+        if (Vector3.Distance(GameManager.instance.player.transform.position, transform.position) <= Reach) {
+            Debug.Log("Ouch!!!");
+            GameManager.instance.playerScript.knockbacked = true;
+            GameManager.instance.playerScript.applyPushback(TotalPunch);
+            GameManager.instance.playerScript.takeDamage(MeleeDamage);
+            StartCoroutine(StopKnockback());
+            AttackTimer = 0;
+        }
+        
+    }
+
+
     void dashattack()
     {
         RaycastHit Detected;
@@ -50,12 +74,14 @@ public class MeleeEnemy : EnemyAI
                 Dir = transform.forward;
                 NewPushPosition = new Vector3 (GameManager.instance.player.transform.position.x, 0, GameManager.instance.player.transform.position.z);
                 Debug.Log("Player Dectected");
-                AttackRange = true; 
             }
-            else
-                  AttackRange = false;
+            
         }
     }
 
-
+    IEnumerator StopKnockback()
+    {
+        yield return new WaitForSeconds(0.7f);
+        GameManager.instance.playerScript.ClearKnockback();
+    }
 }
