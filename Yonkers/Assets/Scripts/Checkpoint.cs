64eb1a3a41211fc2d82ae08beb9116e9e2fc5acf
sosd35 <56140329+sosd35@users.noreply.github.com>
2025-10-21 15:34:06 -0400

    fixed level 2

--- Yonkers/Assets/Scripts/Checkpoint.cs
+++ Yonkers/Assets/Scripts/Checkpoint.cs
@@ -1,78 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class checkpoint : MonoBehaviour
 {
-    [SerializeField] Transform spawnPos;
+    [SerializeField] Renderer model;
 
-    [Tooltip("Models that will have their materials changed.")]
-    [SerializeField] Renderer[] objects;
-
-    [Tooltip("Material the object will change to when touched.")]
-    [SerializeField] Material newMaterial;
-
-    [Tooltip("Allows the object to revert to the previous material.\n\n" +
-        "- All objects will change to the original material of the first object in the list.\n\n" +
-        "- if disabled, the objects will permanently retain the new material.")]
-    [SerializeField] bool canRevertMaterial;
-
-    [Tooltip("Duration of the new material on the object before it reverts back to the original.")]
-    [SerializeField] float materialDuration = 2f;
-
-    //[Tooltip("Duration of the UI Checkpoint Label before it disappears")]
-    //[SerializeField] float labelDuration;
-
-    bool hasTriggered;
-    Material matOrig;
-
-
+    Color colorOrig;
     private void Start()
     {
-        if (objects.Length > 0) matOrig = objects[0].material;
-        else Debug.LogWarning("Please assign an object to the Objects array in the checkpoint.");
+        colorOrig = model.material.color;
     }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !hasTriggered)
+        if (true)
         {
-            hasTriggered = true;
-            if (GameManager.instance.playerSpawn != null)
-            {
-                GameManager.instance.playerSpawn.transform.position = spawnPos.transform.position;
-                GameManager.instance.playerSpawn.transform.rotation = spawnPos.transform.rotation;
-
-                if (canRevertMaterial)
-                {
-                    StartCoroutine(flashMaterial());
-                    // UI Label function here
-                }
-                else
-                {
-                    // UI Label function here
-                    foreach (Renderer model in objects)
-                    {
-                        model.material = newMaterial;
-                    }
-                }
-            }
-            else Debug.LogWarning("Please assign an object with the \"PlayerSpawn\" tag to use checkpoints.");
+            //GameManager.instance.playerSpawnPos.transform.position = transform.position;
+            //StartCoroutine(feedback());
         }
     }
 
-    IEnumerator flashMaterial()
-    {
-        foreach (Renderer model in objects)
-        {
-            model.material = newMaterial;
-        }
-
-        yield return new WaitForSeconds(materialDuration);
-
-        foreach (Renderer model in objects)
-        {
-            model.material = matOrig;
-        }
-    }
+    //IEnumerator feedback()
+    //{
+    //    model.material.color = Color.red;
+    //    GameManager.instance.CheckPointPopup.SetActive(true);
+    //    yield return new WaitForSeconds(0.5f);
+    //    GameManager.instance.CheckPointPopup.SetActive(false);
+    //    model.material.color = colorOrig;
+    //}
 }
