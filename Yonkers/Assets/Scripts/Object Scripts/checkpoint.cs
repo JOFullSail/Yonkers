092259c06285112469e1f0430a682ac908bd1994
sonicgold06 <73099529+sonicgold06@users.noreply.github.com>
2025-10-22 11:50:24 -0400

    Check Point Fixes
    Fixed Checkpoints so they work and aren't bugged .Reworked checkpoint code to only work for flags. Spikes now only do 1 damage.

--- Yonkers/Assets/Scripts/Object Scripts/checkpoint.cs
+++ Yonkers/Assets/Scripts/Object Scripts/checkpoint.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 
 public class checkpoint : MonoBehaviour
 {
-    [SerializeField] Renderer model;
+    [SerializeField] GameObject MainFlagObject;
+    Renderer flagrender1;
+    Renderer flagrender2;
+    Renderer flagrender3;
 
-    Color colorOrig;
+    Color colorOrig1;
+    Color colorOrig2;
+    Color colorOrig3;
     private void Start()
     {
-        colorOrig = model.material.color;
+        GameObject flag = MainFlagObject.transform.Find("Flag").gameObject;
+        flagrender1 = flag.transform.Find("Flag Wrap").GetComponent<Renderer>();
+        flagrender2 = flag.transform.Find("Flag1").GetComponent<Renderer>();
+        flagrender3 = flag.transform.Find("Flag2").GetComponent<Renderer>();
+        colorOrig1 = flagrender1.material.color;
+        colorOrig2 = flagrender2.material.color;
+        colorOrig3 = flagrender3.material.color;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -23,10 +34,14 @@ public class checkpoint : MonoBehaviour
 
     IEnumerator feedback()
     {
-        model.material.color = Color.red;
+        flagrender1.material.color = Color.red;
+        flagrender2.material.color = Color.red;
+        flagrender3.material.color = Color.red;
         //GameManager.instance.CheckPointPopup.SetActive(true);
         yield return new WaitForSeconds(0.5f);
         //GameManager.instance.CheckPointPopup.SetActive(false);
-        model.material.color = colorOrig;
+        flagrender1.material.color = colorOrig1;
+        flagrender2.material.color = colorOrig2;
+        flagrender3.material.color = colorOrig3;
     }
 }
