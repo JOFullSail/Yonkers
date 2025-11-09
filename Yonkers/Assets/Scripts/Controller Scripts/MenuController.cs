6b502b8ddc0a129f093b8e4da4bbc1022bab7bd9
sonicgold06 <73099529+sonicgold06@users.noreply.github.com>
2025-11-09 08:23:51 -0500

    Keyboard inputs on menus work
    Added the ability to use keyboard inputs and give visual feedback when doing so.

--- Yonkers/Assets/Scripts/Controller Scripts/MenuController.cs
+++ Yonkers/Assets/Scripts/Controller Scripts/MenuController.cs
@@ -5,22 +5,11 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField] private GameObject[] Selection;
-    private int index = 0;
-    private void Update()
-    {
-        if (Input.GetButton("arrow_Up"))
-        {
-           
-        }
-    }
+    [SerializeField] private GameObject FirstSelection;
+
     private void OnEnable()
     {
         EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(Selection[index]);
-    }
-    IEnumerator menuslectionDelay()
-    {
-        yield return new WaitForSeconds(0.5f);
+        EventSystem.current.SetSelectedGameObject(FirstSelection);
     }
 }
