fb522df478549c7b9043a9d6e8d5531aeb945b8d
sonicgold06 <73099529+sonicgold06@users.noreply.github.com>
2025-11-09 22:04:01 -0500

    Last fix and build made

--- Yonkers/Assets/Scripts/Helper Scripts/SettingsData.cs
+++ Yonkers/Assets/Scripts/Helper Scripts/SettingsData.cs
@@ -43,7 +43,7 @@ public class SettingsData : MonoBehaviour
 
                     slider.minValue = (int)FOVMin;
                     slider.maxValue = (int)FOVMax;
-                    slider.value = (int)FOVOrig;
+                    slider.value = FOVOrig;
                 }
                 else
                 {
