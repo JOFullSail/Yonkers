813b432f68f11bda7979b9f721423bdcf37a6e79
jpfso <jpaganbaez@student.fullsail.edu>
2025-11-14 17:10:47 -0500

    Finishing pickup touches

--- Yonkers/Assets/Scripts/Object Scripts/Pickup.cs
+++ Yonkers/Assets/Scripts/Object Scripts/Pickup.cs
@@ -22,7 +22,7 @@ public class Pickup : MonoBehaviour
     [Header("For Health Pickups")]
     [SerializeField] int healingAmount = 1;
 
-    Vector3 rotation = new Vector3(0, 0, 100);
+    public Vector3 rotation = new Vector3(0, 0, 100);
 
     private bool wasConsumed = false;
     private void OnTriggerEnter(Collider other)
