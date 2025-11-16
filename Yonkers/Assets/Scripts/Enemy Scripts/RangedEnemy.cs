06b15b7f16457fdd5075d20770a816ac2d9fbc4f
JOFullSail <jtolsen@student.fullsail.edu>
2025-11-16 01:24:26 -0500

    Ranged/Rocket enemy final assets minus VO

--- Yonkers/Assets/Scripts/Enemy Scripts/RangedEnemy.cs
+++ Yonkers/Assets/Scripts/Enemy Scripts/RangedEnemy.cs
@@ -4,12 +4,14 @@ public class RangedEnemy : EnemyAI
 {
     [Header("Ranged Enemy Parameters")]
     [SerializeField] GameObject projectile;
+    [SerializeField] MeshRenderer projectileModel;
     [SerializeField] Transform shootPos;
     [SerializeField] float delayBetweenShots;
-    [SerializeField] AudioSource audEn;
+    [SerializeField] AudioSource shootSoundSource;
     [SerializeField] AudioClip[] audShoot;
     [Range(0, 1)][SerializeField] float audShootVol;
     float shotTimer;
+    float projModelTimer;
 
     bool playerInWeaponRange;
 
@@ -17,6 +19,9 @@ public class RangedEnemy : EnemyAI
     void Update()
     {
         shotTimer += Time.deltaTime;
+        projModelTimer = shotTimer + 0.5f;
+        if (projModelTimer >= delayBetweenShots && projectileModel != null)
+            projectileModel.enabled = true;
         enemyRoutine(); // roamRoutine();
         if (playerDetected && (firstTimeMet && initialAttackDelay <= 0.0f ||
             !firstTimeMet && attackDelayTimer <= 0.0f))
@@ -29,8 +34,11 @@ public class RangedEnemy : EnemyAI
     void shoot()
     {
         animator.SetTrigger("Shoot");
+        projModelTimer = 0;
         shotTimer = 0;
         Instantiate(projectile, shootPos.position, transform.rotation);
-        audEn.PlayOneShot(audShoot[Random.Range(0, audShoot.Length)], audShootVol);
+        shootSoundSource.PlayOneShot(audShoot[Random.Range(0, audShoot.Length)], audShootVol);
+        if(projectileModel != null)
+            projectileModel.enabled = false;
     }
 }
